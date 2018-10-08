f03c1849e225db927cc8e1f776e87220f30997d0
Robert Granlund <info@rockintuit.com>
2018-10-07 19:18:23 -0500

    Third Commit

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  * and open the template in the editor.
  * 
  * Third Commit
+ * 
 */
 ?>
 <style>
@@ -21,6 +22,7 @@
 	animation: beat .35s infinite alternate;
 	transform-origin: center;
 }
+
 
 /* Heart beat animation */
 @keyframes beat{
